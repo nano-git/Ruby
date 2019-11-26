736e1ee5b96165d6a607094558c4ab0cc7700db5
nano-git <piano-bad-atto-continuous@hotmail.co.jp>
2019-11-26 15:38:15 +0900

    1-9

--- index.rb
+++ index.rb
@@ -65,3 +65,16 @@ text = "をマスターしよう"
 puts "HTML" + text
 puts "CSS" + text
 puts "Ruby" + text
+
+# 1-9
+length = 9
+width = 8
+puts width
+puts length * width
+puts "----"
+
+# 変数widthの値を13に変更してください
+width = 13
+
+puts width
+puts length * width

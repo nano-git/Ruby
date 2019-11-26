b467a129face8849806cff73e82c5a599215cd19
nano-git <piano-bad-atto-continuous@hotmail.co.jp>
2019-11-26 15:40:19 +0900

    1-10

--- index.rb
+++ index.rb
@@ -78,3 +78,16 @@ width = 13
 
 puts width
 puts length * width
+
+# 1-10
+length = 9
+width = 8
+puts width
+puts length * width
+
+puts "----"
+# widthの値に5を足して、widthの値を変更してください
+width += 5
+
+puts width
+puts length * width

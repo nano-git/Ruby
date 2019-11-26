70cb41e9b22bc2bb48a03ceb6637533685420a3f
nano-git <piano-bad-atto-continuous@hotmail.co.jp>
2019-11-26 15:50:57 +0900

    1-13

--- index.rb
+++ index.rb
@@ -114,3 +114,17 @@ score = 92
 if score > 80
   puts "よくできました"
 end
+
+# 1-13
+score = 80
+
+# 「score > 80」を出力してください
+puts score > 80
+
+# 「score <= 80」を出力してください
+puts score <= 80
+
+# scoreの値が80以下の場合に、「がんばりましょう」と出力してください
+if score <=80
+  puts "がんばりましょう"
+end

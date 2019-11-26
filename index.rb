cc3f3d56ed45e9a1a2d37fe3e3d924e27938e450
nano-git <piano-bad-atto-continuous@hotmail.co.jp>
2019-11-26 14:59:35 +0900

    1-7

--- index.rb
+++ index.rb
@@ -44,3 +44,15 @@ name = "にんじゃわんこ"
 
 # 変数nameを出力してください
 puts name
+
+# 1-7
+text = "プログラミングを学ぼう"
+
+# 文字列「Progateで」と変数textを連結して、出力してください
+puts "Progateで" + text
+
+length = 8
+width = 9
+
+# 変数lengthと変数widthを掛けて出力してください
+puts length * width
